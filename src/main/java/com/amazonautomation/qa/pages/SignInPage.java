90ab6cfb6e80737d99ea9817dbf2bd70d46139c5
Sandeep Pal <sandeepdtu360@gmail.com>
2021-07-02 14:00:44 +0530

    update pom.xml

--- src/main/java/com/amazonautomation/qa/pages/SignInPage.java
+++ src/main/java/com/amazonautomation/qa/pages/SignInPage.java
@@ -1,9 +1,14 @@
 package com.amazonautomation.qa.pages;
 import com.amazonautomation.qa.base.BaseClass;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class SignInPage extends BaseClass {
 
