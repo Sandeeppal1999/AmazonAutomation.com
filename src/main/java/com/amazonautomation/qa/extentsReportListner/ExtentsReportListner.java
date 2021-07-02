5e1d24684295d1afac7469cca4b3952017d7af05
Sandeep Pal <sandeepdtu360@gmail.com>
2021-07-02 15:23:54 +0530

    update pom.xml

--- src/main/java/com/amazonautomation/qa/extentsReportListner/ExtentsReportListner.java
+++ src/main/java/com/amazonautomation/qa/extentsReportListner/ExtentsReportListner.java
@@ -1,11 +1,9 @@
 package com.amazonautomation.qa.extentsReportListner;
-
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 import org.testng.*;
 import org.testng.xml.XmlSuite;
-
 import java.io.File;
 import java.util.Calendar;
 import java.util.Date;
