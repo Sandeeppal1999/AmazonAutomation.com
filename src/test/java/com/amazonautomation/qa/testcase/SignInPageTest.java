611732bf09622c81347f7905ce93dab1b47486d0
Sandeep Pal <sandeepdtu360@gmail.com>
2021-08-25 17:01:54 +0530

    New Update on project.

--- src/test/java/com/amazonautomation/qa/testcase/SignInPageTest.java
+++ src/test/java/com/amazonautomation/qa/testcase/SignInPageTest.java
@@ -1,34 +1,55 @@
 package com.amazonautomation.qa.testcase;
-
 import com.amazonautomation.qa.base.BaseClass;
 import com.amazonautomation.qa.pages.SignInPage;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 public class SignInPageTest extends BaseClass {
     SignInPage signInPage;
-
     @BeforeMethod
     public void setUp() {
         initialization();
         signInPage = new SignInPage();
     }
-
     @Test
     public void verifySignInNavigation() {
         signInPage.navigateSignInPage();
     }
-
     @Test
-    public void verifySignInLabel() {
+    public void verifySignInLabel()  {
         Assert.assertTrue(signInPage.signInLabel());
     }
-
     @Test
     public void VerifyUserLogin() {
         signInPage.userLogin(prop.getProperty("username"), prop.getProperty("password"));
+    }
+    @Test
+    public void clickOnCreateNewAmazonAccount(){
+        signInPage.createYourNewAmazonAccount();
+    }
+    @Test
+    public void verifySignInAmazonLogo(){
+        signInPage.signLogoAmazon();
+    }
+    @Test
+    public void verifyScrollDown() {
+        signInPage.scrollDownHomePage();
+    } @Test
+    public void verifyNoOfCarousel() {
+        signInPage.noOfCarousel();
+    }
+    @Test
+    public void verifyCarouselClickable() {
+        signInPage.carouselClickable();
+    }
+    @Test
+    public void verifyNeedHelpClickable(){
+        signInPage.needHelp();
+    }
+    @Test
+    public void verifyForgotPassword(){
+        signInPage.needHelp_ForgotPassword();
     }
 
     @AfterMethod
