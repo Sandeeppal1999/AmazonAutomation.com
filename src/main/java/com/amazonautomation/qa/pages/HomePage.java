a2cdfd942f56d68e3c57f436e19630d35aa31747
Sandeep Pal <sandeepdtu360@gmail.com>
2021-08-25 17:07:14 +0530

    New update on project.

--- src/main/java/com/amazonautomation/qa/pages/HomePage.java
+++ src/main/java/com/amazonautomation/qa/pages/HomePage.java
@@ -1,120 +1,82 @@
 package com.amazonautomation.qa.pages;
-
 import com.amazonautomation.qa.base.BaseClass;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
-import java.util.Iterator;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 import java.util.List;
-import java.util.Set;
-
+//HomepageClass
 public class HomePage extends BaseClass {
-
     @FindBy(xpath = "//span[contains(text(),'Hello, manish')]")
-    WebElement loggedUserName;
-
+    WebElement helloUserName;
     @FindBy(linkText = "Amazon Pay")
     WebElement amazonPay;
-
     @FindBy(xpath = "//h2[contains(text(),'Hi, manish')]")
-    WebElement usernameTxt;
-
+    WebElement hiUserName;
     @FindBy(xpath = "//p[contains(text(),'Customer since 2019')]")
     WebElement durationLoggedUser;
-
     @FindBy(xpath = "//span[normalize-space()='Your Orders']")
     WebElement yourOrder;
-
     @FindBy(xpath = "//span[@id='nav-link-accountList-nav-line-1']")
     WebElement accountlist;
     @FindBy(xpath = "//span[normalize-space()='Your Account']")
     WebElement yourAccount;
-
-    @FindBy(xpath = "//*[@id=\"gw-desktop-herotator\"]/div/div/div/div[1]/a/i")
-    WebElement slideIcon;
-
     @FindBy(xpath = "//input[@id='twotabsearchtextbox']")
     WebElement searchTextBox;
-
     @FindBy(xpath = "//input[@id='nav-search-submit-button']")
     WebElement searchTextBoxicon;
-
-    @FindBy(xpath = "//span[contains(text(),'50% Off or more')]")
-    WebElement off50;
-
+    @FindBy(xpath = "//span[contains(text(),'25% Off or more')]")
+    WebElement discountOff25;
     @FindBy(xpath = "//*[@id=\"p_n_is_cod_eligible/4931671031\"]/span/a/div/label/i")
     WebElement eligibleForPayOnDel;
-
     @FindBy(xpath = "//li[@id='p_n_availability/1318485031']//i[@class='a-icon a-icon-checkbox']")
     WebElement includeOutOfStock;
-
-    @FindBy(xpath = "//*[@id=\"p_n_pct-off-with-tax-title\"]/span")
-    WebElement scrollTextCheckBox;
-
-    @FindBy(xpath = "//span[@class='a-size-medium a-color-base a-text-normal'][normalize-space()='Redmi 9 (Sky Blue, 4GB RAM, 64GB Storage)| 5000 mAh| 2.3GHz Mediatek Helio G35 Octa core Processor']")
-    WebElement redmi9;
-
-    @FindBy(xpath = "//*[@id=\"submit.add-to-cart\"]")
-    WebElement addToCart;
-    @FindBy(xpath = "//*[@id=\"attach-sidesheet-view-cart-button\"]/span/input")
-    WebElement navigateCart;
-    @FindBy(xpath = "//img[@alt='Your language is live']")
-    WebElement scrollAmazonInYrLang;
-    @FindBy(xpath = "//span[@class='navFooterBackToTopText']")
-    WebElement backToTopslider;
-    @FindBy(xpath = "//i[@class='a-icon a-icon-previous-rounded']")
-    WebElement carouselButton;
-    @FindBy(xpath = "//div[@class='a-section feed-carousel first-carousel hovering']//span[@class='feed-scrollbar-thumb']")
-    WebElement dealScroller;
     @FindBy(xpath = "//a[@id='nav-logo-sprites']")
     WebElement amazonLogo;
+    @FindBy(xpath = "//div[@data-component-type='s-search-result']")
+    List<WebElement> listOfSearchItem;
+    @FindBy(xpath = "//div[@class='sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 AdHolder sg-col sg-col-4-of-20' ]")
+    List<WebElement> listOfSearchItemHorizontalSpo;
+    @FindBy(xpath = "//div[@class='s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 AdHolder sg-col sg-col-12-of-16' ]")
+    List<WebElement> listOfSearchItemVerticalSpo;
+    @FindBy(xpath = "//span[normalize-space()='Account & Lists']//span[@class='nav-icon nav-arrow']")
+    WebElement getAccountList;
 
     // PageFactory initialization
     public HomePage() {
         PageFactory.initElements(getDriver(), this);
     }
+    //Homepage Method
 
-    public String verifyHomepageTitle() {
-        return driver.getTitle();
+    public String homePageTitle() {
+        new WebDriverWait(getDriver(), Duration.ofSeconds(3)).until(ExpectedConditions.titleIs("Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in"));
+        return getDriver().getTitle();
     }
-
-    public boolean loggedUserName() {
-        boolean logged = loggedUserName.isDisplayed();
-        return logged;
+    public boolean helloUserNameVisible() {
+        return helloUserName.isDisplayed();
     }
-
-    public boolean userNameVisible() {
-        return usernameTxt.isDisplayed();
+    public boolean hiUserNameVisible() {
+        return hiUserName.isDisplayed();
     }
-
     public boolean loggedUserDuration() {
         return durationLoggedUser.isDisplayed();
     }
-
     public void clickOnAmazonPay() {
         amazonPay.click();
     }
-
-    public YourOrderPage clickOnYourOrderPage() {
+    public void clickOnYourOrderPage() {
         Actions action = new Actions(getDriver());
         action.moveToElement(accountlist).build().perform();
         yourOrder.click();
-        return new YourOrderPage();
     }
-
-    public YourAccountPage clickOnYourAccount() {
-        Actions action = new Actions(getDriver()); // action class is used to mouse movement drag and drop right click.
+    public void clickOnYourAccount() {
+        Actions action = new Actions(getDriver());
         action.moveToElement(accountlist).build().perform();
         yourAccount.click();
-        return new YourAccountPage();
-    }
-
-    public void countSlide() {
-        slideIcon.click();
     }
 
     public void searchItem() {
@@ -122,12 +84,71 @@ public class HomePage extends BaseClass {
         searchTextBox.sendKeys(prop.getProperty("searchText"));
         searchTextBoxicon.click();
     }
+    public void listOfSponsoredSearchItem(){
+        searchTextBox.click();
+        searchTextBox.sendKeys(prop.getProperty("searchText"));
+        searchTextBoxicon.click();
 
-    public void off50OrMore() {
-        off50.click();
+        if(listOfSearchItemHorizontalSpo.size()!=0)
+        {   Integer sizeOfList = listOfSearchItemHorizontalSpo.size();
+            System.out.println("<-----No of Searched item found:-" + sizeOfList);
+            List<WebElement> list=listOfSearchItemHorizontalSpo;
+            for (int i = 1; i <list.size() ; i++)
+            {
+                System.out.println("<----------------------------------------------------------------------------------------------------------------------->");
+                WebElement element=list.get(i);
+                String textPro= element.getText();
+                System.out.println("->"+i+" Product of List:"+"\n" +"Name of Product:-"+textPro);
+            }
+        }
+        else {Integer sizeOfList = listOfSearchItemVerticalSpo.size();
+            System.out.println("<-----No of Searched item found:-" + sizeOfList);
+            List<WebElement> list=listOfSearchItemVerticalSpo;
+            for (int i = 1; i <list.size() ; i++)
+            {
+                System.out.println("<----------------------------------------------------------------------------------------------------------------------->");
+                WebElement element=list.get(i);
+                String textPro= element.getText();
+                System.out.println("->"+i+" Product of List:"+"\n" +"Name of Product:-"+textPro);
+            }
+        }
     }
-
-    public void checkboxPayOrstock() {
+    public void listOfSearchItem(){
+        searchTextBox.click();
+        searchTextBox.sendKeys(prop.getProperty("searchText"));
+        searchTextBoxicon.click();
+            Integer sizeOfList = listOfSearchItem.size();
+            System.out.println("<-----No of Searched item found:-" + sizeOfList);
+            List<WebElement> list=listOfSearchItem;
+            for (int i = 1; i <list.size() ; i++)
+           {
+               System.out.println("<----------------------------------------------------------------------------------------------------------------------->");
+               WebElement element=list.get(i);
+               String textPro= element.getText();
+               System.out.println("->"+i+" Product of List:"+"\n" +"Name of Product:-"+textPro);
+           }
+    }
+    public void discount25perOrMore() {
+        searchTextBox.click();
+        searchTextBox.sendKeys(prop.getProperty("searchText"));
+        searchTextBoxicon.click();
+        discountOff25.click();
+        Integer sizeOfList = listOfSearchItem.size();
+        System.out.println("<-----No of Searched item found:-" + sizeOfList);
+        List<WebElement> list=listOfSearchItem;
+        for (int i = 1; i <list.size() ; i++)
+        {
+            System.out.println("<----------------------------------------------------------------------------------------------------------------------->");
+            WebElement element=list.get(i);
+            String textPro= element.getText();
+            System.out.println("->"+i+" Product of List:"+"\n" +"Name of Product:-"+textPro);
+        }
+    }
+    public void selectCheckBoxPayOnDeliveryOutOfStock() {
+        searchTextBox.click();
+        searchTextBox.sendKeys(prop.getProperty("searchText"));
+        searchTextBoxicon.click();
+        discountOff25.click();
         if (eligibleForPayOnDel.isSelected()) {
             System.out.println("CheckBox is Already selected");
         } else {
@@ -139,74 +160,70 @@ public class HomePage extends BaseClass {
             includeOutOfStock.click();
         }
     }
-
-    public String filteredSearchItem() {
-        return redmi9.getText().toString();
-    }
-
-    public void redmi9NewTab() {
-        redmi9.click();
-
-        Set<String> parentTab = getDriver().getWindowHandles();
-        Iterator<String> itr = parentTab.iterator();
-        String mainWindow = itr.next();
-        String childWindow = itr.next();
-        getDriver().switchTo().window(childWindow);
-        System.out.println("Title of child window" + getDriver().getTitle());
-        //driver.close();
-
-        //driver.switchTo().window(mainWindow);
-        //System.out.println("Title of child window"+driver.getTitle());
-    }
-
-    public CartPage addTocartItem() {
-        addToCart.click();
-        return new CartPage();
-    }
-
-    public CartPage navigateCart() {
-        navigateCart.click();
-        return new CartPage();
-    }
-
-    public void scrollDownHomePage() {
-        JavascriptExecutor js = (JavascriptExecutor) getDriver();
-        js.executeScript("arguments[0].scrollIntoView();", scrollAmazonInYrLang);
-    }
-
-    public void backToTop() {
-        backToTopslider.click();
-    }
-
-    public void noOfCarousel() {
-        for (int i = 0; i < 8; i++) {
-            carouselButton.click();
+    public void fetchNthProduct() {
+        searchTextBox.click();
+        searchTextBox.sendKeys(prop.getProperty("searchText"));
+        searchTextBoxicon.click();
+        List<WebElement> list=listOfSearchItem;
+        String nthPro=prop.getProperty("nthProduct");
+        int num = Integer.parseInt(nthPro);
+        for (int i = 1; i < list.size(); i++) {
+            if(i==num)
+            {
+                WebElement element = list.get(i);
+                element.click();
+                String textPro = element.getText();
+                System.out.println("-->" + i + " Product of List:--->" + textPro);
+            }
+            
         }
     }
-
-    public void todayDealsSlider() {
-        JavascriptExecutor js = (JavascriptExecutor) getDriver();
-        js.executeScript("window.scrollBy(0,1000)");
-
-        Actions action = new Actions(getDriver());
-        action.moveToElement(dealScroller).build().perform();
-        action.click();
-        Actions moveSlider = new Actions(getDriver());
-        moveSlider.dragAndDropBy(dealScroller, 790, 0).build().perform();
-
+    public ProductDetailsPage fetchItemDetails()  {
+        searchTextBox.click();
+        searchTextBox.sendKeys(prop.getProperty("searchText"));
+        searchTextBoxicon.click();
+        List<WebElement> list=listOfSearchItem;
+        String nthPro=prop.getProperty("nthProduct");
+        int num = Integer.parseInt(nthPro);
+        for (int i = 1; i < list.size(); i++) {
+            if(i==num) {
+                WebElement element = list.get(i);
+                String textPro = element.getText();
+                System.out.println("-->" + i + " Product of List:--->" + textPro);
+                element.click();
+               // String parentWin=getDriver().getWindowHandle();
+                for(String winHandle:getDriver().getWindowHandles()){
+                  getDriver().switchTo().window(winHandle);
+                }
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return new ProductDetailsPage();
+            }
+        }
+        return null;
     }
-
     public boolean amazonLogoImage() {
         return amazonLogo.isDisplayed();
     }
-
     public void countNoOfLinkOnHomePage() {
         List<WebElement> link = getDriver().findElements(By.tagName("a"));
         System.out.println("No of Link on Login Page:" + link.size());
     }
-
-    public String homePageTitle() {
-        return getDriver().getTitle();
+    public  YourAccountPage navigateYourAccountPage(){
+        Actions action1= new Actions(getDriver());
+        action1.moveToElement(getAccountList).perform();
+        new WebDriverWait(getDriver(), Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(yourAccount));
+        yourAccount.click();
+        return new YourAccountPage();
     }
-
+    public YourOrderPage navigateYourOrderPage() {
+        Actions action1 = new Actions(getDriver());
+        action1.moveToElement(getAccountList).perform();
+        new WebDriverWait(getDriver(), Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(yourOrder));
+        yourOrder.click();
+        return new YourOrderPage();
+    }
 }
--- src/test/java/com/amazonautomation/qa/testcase/HomePageTest.java
+++ src/test/java/com/amazonautomation/qa/testcase/HomePageTest.java
@@ -1,144 +1,99 @@
 package com.amazonautomation.qa.testcase;
-
 import com.amazonautomation.qa.base.BaseClass;
-import com.amazonautomation.qa.pages.CartPage;
 import com.amazonautomation.qa.pages.HomePage;
 import com.amazonautomation.qa.pages.SignInPage;
-import com.amazonautomation.qa.pages.YourOrderPage;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 public class HomePageTest extends BaseClass {
-    SignInPage signInPage;
     HomePage homePage;
-    CartPage cartPage;
-    YourOrderPage yourOrderPage;
-
+    SignInPage signInPage;
     public HomePageTest() {
         super();
     }
-
     @BeforeMethod
     public void setUp() {
         initialization();
-        signInPage = new SignInPage();
         homePage = new HomePage();
-        homePage = signInPage.userLogin(prop.getProperty("username"), prop.getProperty("password"));
+        signInPage=new SignInPage();
+       homePage= signInPage.userLogin(prop.getProperty("username"), prop.getProperty("password"));
     }
-
+    @Test(priority = 1)
+    public void verifyHomePageTitle() {
+        String title = homePage.homePageTitle();
+        System.out.println("-->Title of Home Page"+title);
+        Assert.assertEquals(title,"Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in");
+    }
     @Test
-    public void verifyLoggedUserName() {
-        Assert.assertTrue(homePage.loggedUserName());
+    public void verifyHelloUserName() {
+        Assert.assertTrue(homePage.helloUserNameVisible());
     }
-
     @Test
-    public void verifyUserNameVisible() {
-        Assert.assertTrue(homePage.userNameVisible());
+    public void verifyHiUserName() {
+        Assert.assertTrue(homePage.hiUserNameVisible());
     }
-
     @Test
     public void verifyLoggedUserDuration() {
         Assert.assertTrue(homePage.loggedUserDuration());
     }
-
     @Test
     public void clickOnAmazonPayPage() {
         homePage.clickOnAmazonPay();
     }
-
     @Test
-    public void clickOnYourOrder() {
-        yourOrderPage = homePage.clickOnYourOrderPage();
+    public void verifyClickOnYourOrder() {
+       homePage.clickOnYourOrderPage();
     }
-
     @Test
-    public void verifyYourAccount() {
+    public void verifyClickYourAccount() {
         homePage.clickOnYourAccount();
     }
-
     @Test
     public void verifySearchItem() {
         homePage.searchItem();
     }
-
     @Test
-    public void verify50OffOrMore() {
-        verifySearchItem();
-        homePage.off50OrMore();
+    public void verifyPrintSearchItem(){
+        homePage.listOfSearchItem();
     }
-
     @Test
-    public void verifyCheckBoxStock() {
-        verifySearchItem();
-        homePage.checkboxPayOrstock();
+    public void verifyPrintSponsoredSearchItem(){
+        homePage.listOfSponsoredSearchItem();
     }
-
     @Test
-    public void verifyFilterSearchItem() {
-        verifySearchItem();
-        homePage.checkboxPayOrstock();
-        String text = homePage.filteredSearchItem();
-        Assert.assertEquals(text, "Redmi 9 (Sky Blue, 4GB RAM, 64GB Storage)| 5000 mAh| 2.3GHz Mediatek Helio G35 Octa core Processor");
+    public void verify25PerOffOrMore() {
+        homePage.discount25perOrMore();
     }
-
     @Test
-    public void verifyNavigateNewTab() {
-        verifyFilterSearchItem();
-        homePage.redmi9NewTab();
+    public void verifyCheckBoxPayOnDeliveryOrStock() {
+        homePage.selectCheckBoxPayOnDeliveryOutOfStock();
     }
-
     @Test
-    public void verifyAddToCart() {
-        verifyNavigateNewTab();
-        homePage.addTocartItem();
+    public void verifyFetchNthProduct() {
+        homePage.fetchNthProduct();
     }
-
     @Test
-    public void verifyNavigateToCart() {
-        verifyAddToCart();
-        cartPage = homePage.navigateCart();
+    public void verifyFetchItemDetails() {
+        homePage.fetchItemDetails();
     }
-
     @Test
-    public void verifyScrollDown() {
-        homePage.scrollDownHomePage();
-    }
-
-    @Test
-    public void verifyBackToTop() {
-        homePage.scrollDownHomePage();
-        homePage.backToTop();
-    }
-
-    @Test
-    public void verifyCarouselClickable() {
-        homePage.noOfCarousel();
-    }
-
-    @Test
-    public void verifyTodayDealsSlider() {
-        homePage.todayDealsSlider();
-    }
-
-    @Test
-    public void amazonLogoImageTest() {
+    public void verifyAmazonLogoImage() {
         boolean flag = homePage.amazonLogoImage();
         Assert.assertTrue(flag);
     }
-
     @Test
     public void noOfLinkOnHomePage() {
         homePage.countNoOfLinkOnHomePage();
     }
-
     @Test
-    public void loginPageTitle() {
-        String title = homePage.verifyHomepageTitle();
-        Assert.assertEquals(title, "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in", "HomePage Title not matched");
+    public void verifyNavigateYourAccountPage(){
+        homePage.navigateYourAccountPage();
     }
-
+    @Test
+    public void verifyNavigateYourOrderPage(){
+        homePage.navigateYourOrderPage();
+    }
     @AfterMethod
     public void tearDown() {
         getDriver().quit();
